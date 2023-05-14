7a03cf4727ee1a3682bbedeee057253b0b63e66d
Lizaveta Bokan <117748123+LBokan@users.noreply.github.com>
2023-05-14 20:48:48 +0200

    feat: project init for frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="test">
+    Hello, {{ text }}!
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'world'
+    }
+  }
+}
+</script>
+
+<style>
+.test {
+  color: red;
+}
+</style>
