23d215c88d9c6a27f83bdea0371d6084abba2101
Lizaveta Bokan <117748123+LBokan@users.noreply.github.com>
2023-05-15 22:56:37 +0200

    feat: project init for backend

---
+++ backend/src/test/java/com/exadel/carinsurance/CarinsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exadel.carinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarinsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
