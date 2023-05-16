1cd937bedf7a0c0a73c324547125ff514e3da516
Lizaveta Bokan <117748123+LBokan@users.noreply.github.com>
2023-05-16 18:24:40 +0200

    fix: solve comments

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import LoginPage from '@/views/LoginPage.vue';
-
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/login",
-      component: LoginPage
+      component: () => import('@/views/LoginPage.vue'),
     },
     {
       path: "/",
