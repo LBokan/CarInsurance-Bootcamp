208c29f7bfd9179fcd384ae765ffeb5da26184ed
Lizaveta Bokan <117748123+LBokan@users.noreply.github.com>
2023-06-02 19:35:43 +0200

    feature: add router setup

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -1,21 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { getToken } from '@/helpers/authorization';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/login",
-      component: () => import('@/views/LoginPage.vue'),
+      component: () => import('@/views/LoginPage.vue')
     },
     {
       path: "/",
       component: () => import('@/views/LandingPage.vue'),
+      meta: { requiresAuth: true }
     },
     { 
       path: '/:pathMatch(.*)', 
       component: () => import('@/views/FallbackPage.vue'),
+      meta: { requiresAuth: true }
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !getToken()) {
+    next('/login');
+  } else {
+    next();
+  }
 });
 
 export default router;
